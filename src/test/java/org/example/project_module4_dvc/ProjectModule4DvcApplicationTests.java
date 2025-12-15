435ac4f7fdae6899378903edf1b440d4e620aa15
HoangLeoO <leoo09012001@gmail.com>
2025-12-15 17:48:32 +0700

    new project

---
+++ src/test/java/org/example/project_module4_dvc/ProjectModule4DvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project_module4_dvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectModule4DvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
